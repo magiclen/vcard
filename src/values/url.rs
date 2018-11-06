847e16d405cfa79b9c1a6ecd42e1b14634272fb8
Magic Len <len@magiclen.org>
2018-11-06 21:21:54 +0800

    upgrade validators and use HttpFtpUrlLocalableWithProtocol for URL instead of HttpUrlLocalableWithProtocol

--- src/values/url.rs
+++ src/values/url.rs
@@ -1,9 +1,9 @@
-pub use super::super::validators::http_url::HttpUrlLocalableWithProtocol as URL;
+pub use super::super::validators::http_ftp_url::HttpFtpUrlLocalableWithProtocol as URL;
 use super::*;
 
 impl Value for URL {
     fn fmt(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
-        f.write_str(self.get_full_http_url())?;
+        f.write_str(self.get_full_http_ftp_url())?;
 
         Ok(())
     }
